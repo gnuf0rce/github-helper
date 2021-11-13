7f7188c7fec8439672828dc50df07edce4acb7b4
cssxsh <cssxsh@gmail.com>
2021-11-13 11:53:30 +0800

    fix: GitHubApiException

--- src/main/kotlin/io/github/gnuf0rce/github/exception/GitHubApiException.kt
+++ src/main/kotlin/io/github/gnuf0rce/github/exception/GitHubApiException.kt
@@ -4,10 +4,8 @@ import io.github.gnuf0rce.github.entry.*
 import io.ktor.client.call.*
 import io.ktor.client.features.*
 import kotlinx.coroutines.*
-import kotlinx.serialization.*
 
 class GitHubApiException(override val cause: ClientRequestException) : IllegalStateException(cause) {
-    @OptIn(ExperimentalSerializationApi::class)
     val json: ApiError = runBlocking { cause.response.receive() }
 
     override val message: String get() = json.message
