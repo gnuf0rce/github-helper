5a1ea794545ad53ff619bec881d4fdeb51cdc661
cssxsh <cssxsh@gmail.com>
2022-05-23 20:02:38 +0800

    update: mirai 2.11.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("jvm") version "1.6.0"
-    kotlin("plugin.serialization") version "1.6.0"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.serialization") version "1.6.21"
 
-    id("net.mamoe.mirai-console") version "2.10.3"
+    id("net.mamoe.mirai-console") version "2.11.0"
     id("net.mamoe.maven-central-publish") version "0.7.1"
 }
 
@@ -15,6 +15,7 @@ mavenCentralPublish {
     licenseFromGitHubProject("AGPL-3.0", "master")
     publication {
         artifact(tasks.getByName("buildPlugin"))
+        artifact(tasks.getByName("buildPluginLegacy"))
     }
 }
 
@@ -28,24 +29,24 @@ kotlin {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-client-serialization:1.6.5") {
+    implementation("io.ktor:ktor-client-serialization:1.6.7") {
         exclude(group = "org.jetbrains.kotlin")
         exclude(group = "org.jetbrains.kotlinx")
         exclude(group = "org.slf4j")
         exclude(group = "io.ktor", module = "ktor-client-core")
     }
-    implementation("io.ktor:ktor-client-encoding:1.6.5") {
+    implementation("io.ktor:ktor-client-encoding:1.6.7") {
         exclude(group = "org.jetbrains.kotlin")
         exclude(group = "org.jetbrains.kotlinx")
         exclude(group = "org.slf4j")
         exclude(group = "io.ktor", module = "ktor-client-core")
     }
-    compileOnly("net.mamoe:mirai-core-utils:2.10.3")
+    compileOnly("net.mamoe:mirai-core-utils:2.11.0")
     compileOnly("xyz.cssxsh.mirai:mirai-selenium-plugin:2.0.10")
     compileOnly("xyz.cssxsh.mirai:mirai-administrator:1.0.7")
     // test
-    testRuntimeOnly("xyz.cssxsh.mirai:mirai-selenium-plugin:2.0.10")
-    testImplementation(kotlin("test", "1.6.0"))
+    testRuntimeOnly("xyz.cssxsh.mirai:mirai-selenium-plugin:2.11.0")
+    testImplementation(kotlin("test", "1.6.21"))
 }
 
 tasks {
