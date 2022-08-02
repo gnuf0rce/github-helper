7cd8e88ac4cb5ed135d89b6f7515eb7800178a71
cssxsh <cssxsh@gmail.com>
2022-08-03 01:05:50 +0800

    update: kotlin 1.7.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.7.0"
-    kotlin("plugin.serialization") version "1.7.0"
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
 
     id("net.mamoe.mirai-console") version "2.12.1"
     id("me.him188.maven-central-publish") version "1.0.0-dev-3"
