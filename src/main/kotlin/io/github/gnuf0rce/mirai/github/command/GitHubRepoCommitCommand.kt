26581fe9e2e18e16c0b8cf65de90e5c12d10b450
cssxsh <cssxsh@gmail.com>
2024-05-01 12:55:45 +0800

    fix: repo-commit, close #17

--- src/main/kotlin/io/github/gnuf0rce/mirai/github/command/GitHubRepoCommitCommand.kt
+++ src/main/kotlin/io/github/gnuf0rce/mirai/github/command/GitHubRepoCommitCommand.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2022 dsstudio Technologies and contributors.
+ * Copyright 2021-2024 dsstudio Technologies and contributors.
  *
  *  此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  *  Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
@@ -26,7 +26,8 @@ public object GitHubRepoCommitCommand : CompositeCommand(
         override val tasks: MutableMap<String, GitHubTask> by GitHubRepoTaskData::commits
 
         override suspend fun GitHubTask.load(per: Int, since: OffsetDateTime): List<Commit> {
-            return repo.commits(per = per).filter { it.updatedAt > since }
+            val tree = id.substringAfter(':').ifEmpty { null }
+            return repo.commits(per = per, sha = tree).filter { it.createdAt > since }
         }
     }
 
