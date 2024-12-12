7c2e3386369aa71d0be151a1a21f52143d4e6d0f
cssxsh <cssxsh@gmail.com>
2024-12-12 09:42:37 +0800

    update: ...

--- src/main/kotlin/io/github/gnuf0rce/github/entry/ApiError.kt
+++ src/main/kotlin/io/github/gnuf0rce/github/entry/ApiError.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2022 dsstudio Technologies and contributors.
+ * Copyright 2021-2024 dsstudio Technologies and contributors.
  *
  *  此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  *  Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
@@ -18,6 +18,8 @@ public data class ApiError(
     val documentationUrl: String?,
     @SerialName("message")
     val message: String = "",
+    @SerialName("status")
+    val status: String = "",
     @SerialName("errors")
     val errors: List<Detail> = emptyList()
 ) {
